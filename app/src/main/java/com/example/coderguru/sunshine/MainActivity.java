883973e53d198b898fbbef45f0c79132d4c5ba8f
Prashant <pkb149@hotmail.com>
2016-09-13 20:47:29 +0530

    Version 0.2 Fragments and Adapter (arrayAdapter) implemented

--- app/src/main/java/com/example/coderguru/sunshine/MainActivity.java
+++ app/src/main/java/com/example/coderguru/sunshine/MainActivity.java
@@ -1,38 +1,55 @@
 package com.example.coderguru.sunshine;
 
-import android.app.Activity;
+
 import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import java.lang.reflect.Array;
-
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            Fragment mFragment = new PlaceholderFragment();
+            getFragmentManager().beginTransaction().add(R.id.container,  mFragment).commit();
+        }
     }
 
     public static class PlaceholderFragment extends Fragment {
         public PlaceholderFragment(){
-
         }
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
-            View rootView = inflater.inflate(R.layout.fragment_main, parent, false);
-            String[] arr={"Today - cloudy - 27/21","Tomorrow - cloudy - 27/21","Thrusday - cloudy - 27/21","Friday - cloudy - 27/21",
-                    "Saturday - cloudy - 27/21","Sunday - cloudy - 27/21","Monday - cloudy - 27/21","Tuesday - cloudy - 27/21"};
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
-            ArrayAdapter adapter = new ArrayAdapter(getActivity(),R.layout.list_item_forecast, R.id.listitemforecast_textview,arr);
+            String[] arr={
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(arr));
+
+            ArrayAdapter mForecastAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.listitemforecast_textview,
+                    weekForecast);
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             ListView listView= (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(adapter);
+            listView.setAdapter(mForecastAdapter);
             return rootView;
         }
     }
